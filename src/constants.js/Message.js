2b0aab16fb70863419d07259e8f11bdc63c5fb7d
dangtrungdev113999 <dangtrungdev113999@gmail.com>
2019-07-15 22:42:57 +0700

    connect to redux

---
+++ src/constants.js/Message.js
@@ -0,0 +1,4 @@
+export const MSG_ADD_TO_CART_SUCCESS = 'thêm vào giỏ hàng thành công';
+export const MSG_UPDATE_CART_SUCCESS = 'cập nhật giỏ hàng thành công!';
+export const MSG_DELETE_PRODUCT_IN_CART_SUCCESS = 'chưa có sản phẩm nào trong giỏ hàng';
+export const MSG_WELCOME = 'chào mừng bạn đén với shopping online';
